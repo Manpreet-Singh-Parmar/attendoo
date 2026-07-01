1626045347a02f49f5e9536556526ca798572c6f
Manpreet-Singh-Parmar <mspquanta@gmail.com>
2026-07-01 22:50:01 +0530

    idk

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://attendooo.com',
 	adapter: cloudflare(),
 	devToolbar: {
 		enabled: false,
